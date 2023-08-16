81d5df4fa0e22a10a7674447e1e8f562ec2d33b6
Manny <manny@192.168.1.2>
2023-08-16 11:34:48 +0700

    Fixing submit job command

--- src/model/train.py
+++ src/model/train.py
@@ -3,15 +3,19 @@
 import argparse
 import glob
 import os
+import numpy as np
 
 import pandas as pd
+import mlflow
+
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
+    mlflow.sklearn.autolog()
 
 
     # read data
@@ -23,6 +27,9 @@ def main(args):
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
+    autolog_run = mlflow.last_active_run()
+    print(autolog_run)
+
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -33,13 +40,14 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
-
-
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def parse_args():
     # setup arg parser
